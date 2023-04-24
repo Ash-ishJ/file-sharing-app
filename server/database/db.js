83aeb11e0c8b90ae53b1e537348d3a2f7bd6f838
Ash-ishJ <ashish731824@gmail.com>
2023-04-24 22:34:48 +0530

    added credentials to .env

--- server/database/db.js
+++ server/database/db.js
@@ -4,9 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const DBConnection = async () => {
-    
+    const userName = process.env.DB_USERNAME;
+    const password = process.env.DB_PASSWORD;
 
-    const MONGO_URI = `mongodb://ashish0401:9211@ac-fbmobgd-shard-00-00.q6jo9zj.mongodb.net:27017,ac-fbmobgd-shard-00-01.q6jo9zj.mongodb.net:27017,ac-fbmobgd-shard-00-02.q6jo9zj.mongodb.net:27017/?ssl=true&replicaSet=atlas-a15hyl-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    const MONGO_URI = `mongodb://${userName}:${password}@ac-fbmobgd-shard-00-00.q6jo9zj.mongodb.net:27017,ac-fbmobgd-shard-00-01.q6jo9zj.mongodb.net:27017,ac-fbmobgd-shard-00-02.q6jo9zj.mongodb.net:27017/?ssl=true&replicaSet=atlas-a15hyl-shard-0&authSource=admin&retryWrites=true&w=majority`;
     try {
         await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
         console.log('Database connected successfully');
